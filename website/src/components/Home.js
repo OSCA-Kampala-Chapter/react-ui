c5764ecc523bd92ecdaad36c39d2d9c02ca6a796
HassanBahati <mukisabahati@gmail.com>
2023-01-06 01:21:30 +0300

    refactor: separate home component from homepage

---
+++ website/src/components/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+
+import styles from "../pages/index.module.css";
+
+import CodeBlock from "@theme/CodeBlock";
+
+const Home = () => {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Get Started
+          </Link>
+        </div>
+        <div>
+          <CodeBlock className="language-bash">{`npm i @reactui-org/core --save`}</CodeBlock>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Home;
