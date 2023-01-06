04df74c137f03fa86b82e4f3df2e0e58f9c16152
HassanBahati <mukisabahati@gmail.com>
2023-01-06 12:01:12 +0300

    chore: configure tailwindcss

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -52,7 +52,19 @@ const config = {
       }),
     ],
   ],
-
+  plugins: [
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -14,16 +14,18 @@ const Home = () => {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Get Started
-          </Link>
-        </div>
-        <div>
-          <CodeBlock className="language-bash">{`npm i @reactui-org/core --save`}</CodeBlock>
+        <div className="flex items-center">
+          <div className="grid place-items-center">
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro"
+            >
+              Get Started
+            </Link>
+          </div>
+          <div className="w-1/3">
+            <CodeBlock className="language-bash">{`npm i @reactui-org/core --save`}</CodeBlock>
+          </div>
         </div>
       </div>
     </header>
