2168b9b533854530d483b6c4b940185f25bee0ea
HassanBahati <mukisabahati@gmail.com>
2023-01-16 16:23:27 +0300

    initial commit

---
+++ website/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
+      <main>
+        <h1 className="font-mono text-xl code">
+          Welcome to <span className="text-purple-700">Nextjs</span> and{" "}
+          <span className="text-indigo-700">TailwindCSS</span>
+        </h1>
+      </main>
+    </div>
+  );
+}
