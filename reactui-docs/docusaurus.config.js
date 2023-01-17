550a6c1445f2a705d4c1e6d3fcf3cbfddc715ffd
Rishav Raj <97666287+Rishav1707@users.noreply.github.com>
2023-01-17 14:35:42 +0530

    `react-ui` github link added

--- reactui-docs/docusaurus.config.js
+++ reactui-docs/docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/OSCA-Kampala-Chapter/react-ui",
             label: "GitHub",
             position: "right",
           },
