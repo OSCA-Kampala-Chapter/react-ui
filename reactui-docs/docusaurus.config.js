730ea0cd981515b643d40868e33aeb52a398e975
unknown <madhavsetia24@gmail.com>
2023-01-18 14:42:02 +0530

    links updated in required file

--- reactui-docs/docusaurus.config.js
+++ reactui-docs/docusaurus.config.js
@@ -39,14 +39,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/OSCA-Kampala-Chapter/react-ui",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/OSCA-Kampala-Chapter/react-ui",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -119,7 +119,7 @@ const config = {
               },
               {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/OSCA-Kampala-Chapter/react-ui",
               },
             ],
           },
