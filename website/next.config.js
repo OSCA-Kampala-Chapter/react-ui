8c7be6c2bb5d16f2da7388ba99b8ad10b2b58c52
HassanBahati <mukisabahati@gmail.com>
2023-02-14 11:12:33 +0300

    chore: add withNextra config to next.config.js

--- website/next.config.js
+++ website/next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+module.exports = withNextra(nextConfig);
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
