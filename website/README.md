d3ebaff39201c8b5705622ebabe3369557c5da11
HassanBahati <mukisabahati@gmail.com>
2023-02-14 13:07:26 +0300

    chore: intial commit

---
+++ website/README.md
@@ -0,0 +1,15 @@
+# React UI Website
+
+The React Components Library
+
+## Quick Start
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ website/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>React UI</span>,
+  project: {
+    link: "https://github.com/OSCA-Kampala-Chapter/react-ui",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/OSCA-Kampala-Chapter/react-ui",
+  footer: {
+    text: "React UI docs",
+  },
+};
+
+export default config;
